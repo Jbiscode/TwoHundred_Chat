1011a70130db91c860a10eac2c650b6ce367fe60
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-05-22 00:34:51 +0900

    React Router 추가하고 기본링크연결 api프록시 설정하고, 기본포트 변경

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from './pages/login/Login';
-// import Home from "./pages/home/Home";
-// import SignUp from './pages/signup/SignUp';
+import Home from "./pages/home/Home";
+import SignUp from './pages/signup/SignUp';
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} /> 
+      </Routes>
     </div>
   );
 }
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
 
 const SignUp = () => {
@@ -47,9 +48,9 @@ const SignUp = () => {
 
 					<GenderCheckbox />
 
-					<a className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block' href='#'>
+					<Link to={'/login'} className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block' href='#'>
 						Already have an account?
-					</a>
+					</Link>
 
 					<div>
 						<button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
