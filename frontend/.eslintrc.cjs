92505dd3abed301a6cbdc4151c6ac988ba612ea1
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-05-22 01:31:37 +0900

    회원가입&검증 로직 구현, useState, 커스텀훅 제작,Toaster 사용

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -17,5 +17,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    // Disable prop-types rule
+    "react/prop-types": "off",
   },
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Login from './pages/login/Login';
 import Home from "./pages/home/Home";
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} /> 
       </Routes>
+      <Toaster />
     </div>
   );
 }
