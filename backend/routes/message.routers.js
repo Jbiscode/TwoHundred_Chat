eb0d59ffc5f814a37e15828a2d19d827ac719f70
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-06-11 10:40:57 +0900

    FIX : 메시지 송수신 라우터 수정

--- backend/routes/message.routers.js
+++ backend/routes/message.routers.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { sendMessage, getMessages } from '../controller/message.controller.js';
+import { sendMessage, getMessages, getLastAndUnreadMessages } from '../controller/message.controller.js';
 import protectRoute from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
-router.post('/send/:id',protectRoute, sendMessage); // protectRoute는 미들웨어로서, sendMessage,getMessages 함수를 실행하기 전에 먼저 실행됨
-router.get('/get/:id',protectRoute, getMessages);
+router.post('/send/:roomId/:userId',protectRoute, sendMessage); // protectRoute는 미들웨어로서, sendMessage,getMessages 함수를 실행하기 전에 먼저 실행됨
+router.get('/get/:roomId/:userId',protectRoute, getMessages);
+router.get('/lastAndUnread/:roomId/:userId',protectRoute, getLastAndUnreadMessages);
 
 export default router;
+
